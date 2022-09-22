ba9b3d9d09614571cde5292cec79b68b51a5d78f
unknown <aquinasmoriasi@gmail.com>
2022-09-22 12:48:12 +0300

    add owners and species tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,20 @@ CREATE TABLE animals (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE owners (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name varchar(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id) ON DELETE CASCADE;
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id) ON DELETE CASCADE;
