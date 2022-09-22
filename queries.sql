b40b7c22f93cd970a4f277b87d19b90d4b78f5fc
unknown <aquinasmoriasi@gmail.com>
2022-09-22 13:45:07 +0300

    update owners and species

--- queries.sql
+++ queries.sql
@@ -9,24 +9,6 @@ SELECT * FROM animals WHERE neutered =  true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
-BEGIN;
-UPDATE animals SET species = 'unspecified';
-ROLLBACK;
-BEGIN;
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
-COMMIT;
-BEGIN;
-DELETE FROM animals;
-ROLLBACK;
-BEGIN;
-DELETE FROM animals WHERE date_of_birth > '2022-01-01';
-SAVEPOINT point1;
-UPDATE animals SET weight_kg = weight_kg * -1;
-ROLLBACK;
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-COMMIT;
-
 SELECT COUNT (*) FROM animals;
 SELECT COUNT (*) FROM animals WHERE escape_attempts = 0;
 SELECT AVG ( weight_kg ) FROM animals;
